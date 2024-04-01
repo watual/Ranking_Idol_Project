86a594f566d2d21ca633842e9c82ee68cb7586a4
PokeMasterLEE <iyeojae@gmail.com>
2024-04-01 18:24:59 +0900

    1
    1

---
+++ Ranking_idol_Project/src/main/java/com/example/ranking_idol_project/RankingIdolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ranking_idol_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingIdolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankingIdolProjectApplication.class, args);
+	}
+
+}
---
+++ Ranking_idol_Project/src/test/java/com/example/ranking_idol_project/RankingIdolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ranking_idol_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingIdolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
