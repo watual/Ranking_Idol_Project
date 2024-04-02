ea6f9df06d7c87c4187cc5c2086c8c3361d536cd
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-02 20:01:33 +0900

    실행가능

--- Ranking_Idol_Project/src/main/java/org/project/ranking_idol_project/service/MemberService.java
+++ Ranking_Idol_Project/src/main/java/org/project/ranking_idol_project/service/MemberService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private final MemberRepository memberRepository;
+    //private final MemberRepository memberRepository;
     public void save(MemberDTO memberDTO) {
     }
 }
