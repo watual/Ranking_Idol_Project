4bd81dd460a9c561c6ee376ff62a70d24af10a0c
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-06 01:26:39 +0900

    수리

--- 정부24/baseScript.js
+++ 정부24/baseScript.js
@@ -1,5 +1,32 @@
 
+// // Firebase SDK 라이브러리 가져오기
+//import initializeApp from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+// import { doc, deleteDoc, query, limit, collection, addDoc, orderBy, getDocs, getDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
+
+// Firebase 구성 정보 설정
+const firebaseConfig = {
+    apiKey: "AIzaSyDUo3wGBYmzDE0daVh0twAmzZ2r8TzyrGE",
+    authDomain: "watual-f9224.firebaseapp.com",
+    projectId: "watual-f9224",
+    storageBucket: "watual-f9224.appspot.com",
+    messagingSenderId: "307021915043",
+    appId: "1:307021915043:web:ec35f5002d695d81a1f016",
+    measurementId: "G-248XS4LSEE"
+};
+
+// // Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+
+let asdf123 = document.getElementById('helloas');
+asdf123.addEventListener('click', function (event){
+    alert('asdfasdf'+event.target.value);
+})
+
+fetchDataAndRender();
 $("#pressthis2").click(async function () {
     alert("asdfasdf");
 });
@@ -56,3 +83,7 @@ fetch(url).then(res => res.json()).then(data => {
 $("#savebtn").click(async function () {
     $('#postingbox').toggle();
 });
+
+module.exports = {
+    
+}
